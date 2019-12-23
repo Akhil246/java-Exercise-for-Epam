d055370a56cc6b511bbcb831899f00b925faae29
Akhil246 <56591557+Akhil246@users.noreply.github.com>
2019-12-23 16:10:36 +0530

    Add files via upload

---
+++ Positivecheck.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Positivecheck
+{
+    public static void main(String args[]) 
+    
+{
+        Scanner s = new Scanner(System.in);
+        
+int a=s.nextInt();
+       
+ if(a>0)
+        
+{
+            System.out.println("positive number");
+     
+   }
+        
+else
+      
+  {
+           
+ System.out.println("positive number");
+      
+  }
+     
+   
+
+ }
+
+}
